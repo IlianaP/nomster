a8a5c2cf5e071016e00dc7e4c3b2681642b8737c
IlianaP <ip97@cornell.edu>
2016-07-15 17:49:06 +0000

    Sending out emails

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base 
 	belongs_to :place 
 	belongs_to :user 
-	mount_uploader :photo, PictureUploader
-
+	mount_uploader :picture, PictureUploader
 end
+
--- db/migrate/20160715162457_add_picture_to_photos.rb
+++ db/migrate/20160715162457_add_picture_to_photos.rb
@@ -1,5 +1,6 @@
 class AddPictureToPhotos < ActiveRecord::Migration
   def change
-    add_column :photos, :picture, :string
+    add_column :photos, :picture, :string 
+    
   end
 end
