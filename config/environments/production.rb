1a73c2ee54e315eb6ede9a3c2001bcf4593dde2f
IlianaP <ip97@cornell.edu>
2016-07-18 22:19:04 +0000

    Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Nomster::Application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  config.action_mailer.default_url_options = { host: 'nomster-iliana-paleva.herokuapp.com' } 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
